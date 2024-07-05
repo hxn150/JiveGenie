b86be794e70b34e1ba48eab6845527123ed60cdc
Helen Nguyen <hxn150@case.edu>
2024-07-05 08:50:28 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+
+import JiveGenie from "@/pages/index";
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<JiveGenie />} path="/" />
+    </Routes>
+  );
+}
+
+export default App;
+ 
